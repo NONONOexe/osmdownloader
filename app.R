b64096361adbbd7d8819eec47077cd0100a0128d
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2023-11-02 07:42:16 +0900

    Remove unuse files

--- app.R
+++
@@ -1,9 +0,0 @@
-# Install packages
-options(repos = "https://cran.rstudio.com/")
-if (!suppressWarnings(require(pacman, quietly = TRUE))) {
-  install.packages("pacman")
-}
-pacman::p_load("shiny", "leaflet", "sf", "geosphere", "osmdata")  
-
-# Run application
-runApp(appDir = ".", launch.browser = TRUE)
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-Rscript.exe app.R
